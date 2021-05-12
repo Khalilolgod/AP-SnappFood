b810cd25785993094a17cd0a43d6e44733c8b717
ebi <tmpmailebi@gmail.com>
2021-05-13 03:14:27 +0430

    OperatorMenu was sorted out

--- src/main/java/ir/ac/kntu/Menu.java
+++ src/main/java/ir/ac/kntu/Menu.java
@@ -1,4 +1,34 @@
 package ir.ac.kntu;
 
-public class Menu {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public abstract class Menu {
+
+    private String filePath;
+
+    private File file;
+
+    public Menu(String filePath) {
+        this.filePath = "src/main/java/ir/ac/kntu/menus/"+filePath;
+        this.file = new File(this.filePath);
+    }
+
+    public void showMenu(){
+        try {
+            Scanner myReader = new Scanner(this.file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public abstract boolean inputProcessor(Agency agency);
+
 }
---
+++ src/main/java/ir/ac/kntu/OperatorMenu.java
@@ -0,0 +1,31 @@
+package ir.ac.kntu;
+
+public class OperatorMenu extends Menu {
+
+    OperatorMenu(){
+        super("OperatorMenu.txt");
+    }
+
+    public void execute(Agency agency){
+        showMenu();
+        inputProcessor(agency);
+    }
+
+    @Override
+    public boolean inputProcessor(Agency agency) {
+        String choice = ScannerWrapper.getInstance().nextLine();
+        switch (choice){
+            case "a":
+                System.out.println("ok");
+                agency.takeOrder();
+                return true;
+            case "b":
+                System.out.println("fuck you");
+                break;
+            default:
+                System.out.println("fuck you");
+                return false;
+        }
+        return false;
+    }
+}
