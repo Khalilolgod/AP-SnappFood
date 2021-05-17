463dd6df86ecd5b7bddca6204995be9c65d3131e
IbrahimSheikh <eyebiman@gmail.com>
2021-05-18 02:53:53 +0430

    Added RandObjGen->shiftGen,scheduleGen,stringGen,generate

--- src/main/java/ir/ac/kntu/RandObjGen.java
+++ src/main/java/ir/ac/kntu/RandObjGen.java
@@ -33,6 +33,76 @@ public class RandObjGen {
         return new Delivery(vehicleType, wageType, restaurants, schedule);
     }
 
+    /**
+     * @return a random Shift object
+     */
+    public Shift shiftGen() {
+        int hour1 = rand.nextInt(24);
+        int minute1 = rand.nextInt(60);
+        int deltaHour = rand.nextInt(2);
+        int deltaMinute = rand.nextInt(59) + 1;
+        int hour2 = Math.min(hour1 + deltaHour, 23);
+        int minute2 = Math.min(minute1 + deltaMinute, 59);
+        LocalTime start = LocalTime.of(hour1, minute1);
+        LocalTime end = LocalTime.of(hour2, minute2);
+        return new Shift(start, end);
+    }
+
+    /**
+     * @return a random Schedule object
+     */
+    public Schedule scheduleGen() {
+        int daysInWeek = rand.nextInt(7) + 1;
+        int numberOfshifts;
+        DayOfWeek dayOfTheWeek;
+        ArrayList<WorkDay> workDays = new ArrayList<>();
+        for (int i = 0; i < daysInWeek; i++) {
+            dayOfTheWeek = DayOfWeek.values()[rand.nextInt(DayOfWeek.values().length)];
+            numberOfshifts = rand.nextInt(2) + 1;
+            ArrayList<Shift> shifts = new ArrayList<>();
+            for (int s = 0; s < numberOfshifts; s++) {
+                Shift shift = shiftGen();
+                shifts.add(shift);
+            }
+            WorkDay workDay = new WorkDay(dayOfTheWeek, shifts);
+            workDays.add(workDay);
+        }
+        return new Schedule(workDays);
+    }
+
+    public String stringGen(int n) {
+        String alphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "qwertyuiopasdfghjklzxcvbnm";
+        StringBuilder sb = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+
+            int index = (int) (alphaNumericString.length() * Math.random());
+            sb.append(alphaNumericString.charAt(index));
+        }
+        return sb.toString();
+    }
+
+
+    public FoodMenu foodmenuGen() {
+
+    }
+
+    public Food foodGen(String name){
+
+    }
+
+    public Costumer costumerGen() {
+
+    }
+
+
+    public void generate(Agency agency) {
+        for (int i = 0; i < 15; i++) {
+            agency.getAllDeliveries().add(deliveryGen());
+        }
+        for (int i = 0; i < 5; i++) {
+            agency.getRestaurants().add(restaurentGen(restaurentNames[i]));
+        }
+    }
 
 
 }
