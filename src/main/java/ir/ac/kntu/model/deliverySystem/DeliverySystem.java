7628425ca6c689c1b3f17610bbbc91c7da4b7a04
IbrahimSheikh <eyebiman@gmail.com>
2021-06-07 02:21:15 +0430

    added DeliverySystem->findDeliverer

--- src/main/java/ir/ac/kntu/model/deliverySystem/DeliverySystem.java
+++ src/main/java/ir/ac/kntu/model/deliverySystem/DeliverySystem.java
@@ -1,24 +1,41 @@
 package ir.ac.kntu.model.deliverySystem;
 
+import ir.ac.kntu.model.services.Provider;
+import ir.ac.kntu.model.users.Costumer;
+
 import java.util.ArrayList;
 
 public class DeliverySystem
 {
-    private ArrayList<Deliverer> deliverers;
+    private static ArrayList<Deliverer> deliverers;
 
     public DeliverySystem(ArrayList<Deliverer> deliverers) {
-        this.deliverers = deliverers;
+        DeliverySystem.deliverers = deliverers;
     }
 
     public DeliverySystem(){
-        this.deliverers = new ArrayList<>();
+        deliverers = new ArrayList<>();
     }
 
-    public ArrayList<Deliverer> getDeliverers() {
+    public static Deliverer findDeliverer(Costumer costumer , Provider provider){
+        double minDist = Double.MAX_VALUE;
+        double dist = 0;
+        Deliverer deliverer =  null;
+        for(Deliverer d : getDeliverers()){
+            dist = d.getLocation().distanceFrom(costumer.getLocation()) + d.getLocation().distanceFrom(provider.getLocation());
+            if (dist<minDist){
+                minDist = dist;
+                deliverer = d;
+            }
+        }
+        return deliverer;
+    }
+
+    private static ArrayList<Deliverer> getDeliverers() {
         return deliverers;
     }
 
     public void setDeliverers(ArrayList<Deliverer> deliverers) {
-        this.deliverers = deliverers;
+        DeliverySystem.deliverers = deliverers;
     }
 }
